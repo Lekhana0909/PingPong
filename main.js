129aaa6ae5445a6ed9fa574732721f8831533dbb
Lekhana0909 <74604087+Lekhana0909@users.noreply.github.com>
2021-02-23 19:55:13 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -24,8 +24,26 @@ var ball = {
 function setup(){
   var canvas =  createCanvas(700,600);
   canvas.parent('canvas');
+	video=createCapture(VIDEO);
+	video.size(800,400);
+	video.parent("game_console");
+
+	poseNet= ml5.poseNet(video, modelLoaded);
+	poseNet.on('pose', gotPoses);
 }
 
+function modelLoaded(){
+	console.log("model loaded!");
+}
+
+ function gotPoses(results){
+if (results.length>0){
+	console.log(results);
+	noseX=results[0].pose.nose.x;
+	noseY=results[0].pose.nose.y;
+	
+ }
+ }
 
 function draw(){
 
